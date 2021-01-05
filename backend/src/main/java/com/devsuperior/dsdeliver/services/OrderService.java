50bb82623a44bd3ff425e04dbd6924ad69208046
Thiago Alves <thiago_marketingdigital@hotmail.com>
2021-01-05 18:24:03 -0300

    PUT orders

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -30,17 +30,27 @@ public class OrderService {
 		return orderRepository.findOrderWithProducts().stream().map(OrderDTO::new).collect(Collectors.toList());
 	}
 	
+	@Transactional
 	public OrderDTO insert(OrderDTO dto) {
 		
 		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
 		
 		for(ProductDTO p : dto.getProducts()) {			
-			Product product = productRepository.findById(p.getId()).get(); //getOne não funciona aqui.
+			Product product = productRepository.findById(p.getId()).get(); // -> getOne não funcionou comigo :(
 			order.getProducts().add(product);
 		}
 		
 		order = orderRepository.save(order);
 		
+		return new OrderDTO(order);
+	}
+	
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		//Order order = orderRepository.getOne(id); // -> getOne não funcionou comigo :(
+		Order order = orderRepository.findById(id).get();
+		order.setStatus(OrderStatus.DELIVERED);
+		order = orderRepository.save(order);
 		return new OrderDTO(order);
 	}
 }
