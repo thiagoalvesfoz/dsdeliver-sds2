b765704b24db2e7b66c43ac02c624d33029e4452
Thiago Alves <thiago_marketingdigital@hotmail.com>
2021-01-08 19:37:16 -0300

    finish front-web

--- front-web/src/pages/orders/index.tsx
+++ front-web/src/pages/orders/index.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import { OrderLocationData, Product } from "../../services/types";
 import { checkIsSelected } from "../../services/helpers";
-import { fetchProducts } from "../../services/api";
+import { fetchProducts, saveOrder } from "../../services/api";
 
 //components
+import { toast } from "react-toastify";
 import OrderLocation from "../../components/order-location";
 import OrderSummary from "../../components/order-summary";
 import ProductsList from "../../components/products-list";
@@ -15,12 +16,12 @@ import "./styles.css";
 export default function Orders() {
   const [products, setProducts] = useState<Product[]>([]);
   const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
-  const [, setOrderLocation] = useState<OrderLocationData>();
+  const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
 
   useEffect(() => {
     fetchProducts()
       .then((res) => setProducts(res.data))
-      .catch((err) => console.log(err));
+      .catch(() => toast.warning("Erro ao listar produtos"));
   }, []);
 
   const totalPrice = selectedProducts.reduce((sum, item) => {
@@ -40,6 +41,21 @@ export default function Orders() {
     }
   };
 
+  const handleSubmit = () => {
+    const productsIds = selectedProducts.map(({ id }) => ({ id }));
+    const payload = {
+      ...orderLocation!,
+      products: productsIds,
+    };
+
+    saveOrder(payload)
+      .then(({ data }) => {
+        toast.error(`Pedido enviado com sucesso! Nº ${data.id}`);
+        setSelectedProducts([]);
+      })
+      .catch(() => toast.warning("Erro ao enviar pedido"));
+  };
+
   return (
     <div className="orders-container">
       <StepsHeader />
@@ -51,7 +67,11 @@ export default function Orders() {
       <OrderLocation
         onChangeLocation={(location) => setOrderLocation(location)}
       />
-      <OrderSummary amount={selectedProducts.length} totalPrice={totalPrice} />
+      <OrderSummary
+        amount={selectedProducts.length}
+        totalPrice={totalPrice}
+        onSubmit={handleSubmit}
+      />
     </div>
   );
 }
