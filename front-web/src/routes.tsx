3d318a1051feb3b6ed0855fa17a01f25740ffdde
Thiago Alves <thiago_marketingdigital@hotmail.com>
2021-01-08 19:57:28 -0300

    refactoring api and routes

--- front-web/src/routes.tsx
+++ front-web/src/routes.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
 
 //component
-import Navbar from "./components/navbar";
+import HeaderNavbar from "./components/navbar";
 import Footer from "./components/footer";
 
 //pages
@@ -34,7 +34,7 @@ export default function routes() {
   return (
     <BrowserRouter>
       <AnimatePresence>
-        <Navbar />
+        <HeaderNavbar />
         <motion.div
           initial="initial"
           animate="in"
